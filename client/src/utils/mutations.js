8e9989621190847448e4e16ded7ce9214a4f0513
jcg0 <jg.awsprod@gmail.com>
2023-04-05 00:18:48 -0400

    got the server finally working

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-  mutation Login($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
@@ -13,29 +13,19 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation AddUser($username: String!, $email: String!, $password: String!) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
-        username
-        email
         _id
-        bookCount
-        savedBooks {
-          authors
-          bookId
-          image
-          link
-          title
-          description
-        }
+        username
       }
     }
   }
 `;
 
 export const SAVE_BOOK = gql`
-  mutation SavedBook(
+  mutation savedBook(
     $description: String!
     $title: String!
     $bookId: ID!
@@ -68,7 +58,7 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation RemoveBook($bookId: ID!) {
+  mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
       _id
       username
